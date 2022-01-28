bc4e8ffbe8ff4a422374e2092e5802793111fb44
keimori <keimori@aliyun.com>
2022-01-28 17:41:35 +0800

    Create s3-bucket.tf

---
+++ s3-bucket.tf
@@ -0,0 +1,8 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/ONAP/s3-bucket/aws"
+  version = "2.8.0"
+  # insert required variables here
+  
+  bucket = "my-s3-bucket"
+  bucket_prefix = var.prefix
+}
